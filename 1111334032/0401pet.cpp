e82b4444193b8d7fc5a3e948e297b93dc50c2648
chang-long32 <jordan981004@gmail.com>
2026-04-01 15:11:21 +0800

    Create 0401pet.cpp

---
+++ 1111334032/0401pet.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "pet.h"
+using namespace std;
+
+// 建構元
+Pet::Pet(string n, int a) {
+    setName(n);
+    setAge(a);
+    cout << "建立寵物：" << name << endl;
+}
+
+// 解構元
+Pet::~Pet() {
+    cout << "與寵物 " << name << " 再見！" << endl;
+}
+
+// setName
+void Pet::setName(string n) {
+    if (n.length() > 2 && n.length() < 5) {
+        name = n;
+    }
+    else {
+        cout << "姓名長度錯誤！設定為預設值 Dog" << endl;
+        name = "Dog";
+    }
+}
+
+// setAge
+void Pet::setAge(int a) {
+    if (a <= 20) {
+        age = a;
+    }
+    else {
+        cout << "年齡錯誤！設定為預設值 1" << endl;
+        age = 1;
+    }
+}
+
+// getInfo
+void Pet::getInfo() {
+    cout << "姓名：" << name << "，年齡：" << age << endl;
+}
