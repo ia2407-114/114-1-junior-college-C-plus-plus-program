705b3bc4eefd487eb77905cdbbb9f5520e39a3af
bobolinlin48900 <benny540424@gmail.com>
2026-04-15 16:09:50 +0800

    Create 0415Date.h

---
+++ 1111334026/0415Date.h
@@ -0,0 +1,34 @@
+// Fig. 9.4: Time.h
+// Time class containing a constructor with default arguments.
+// Member functions defined in Time.cpp.
+
+// prevent multiple inclusions of header 
+#ifndef DATE_H
+#define DATE_H
+
+// Time class definition
+class Date
+{
+public:
+	explicit Date(int = 1900, int = 1, int = 1); // default constructor
+
+	// set functions
+	void setDate(int, int, int); // set hour, minute, second
+	void setYear(int); // set hour (after validation)
+	void setMonth(int); // set minute (after validation)
+	void setDay(int); // set second (after validation)
+
+	// get functions
+	unsigned int getYear() const; // return hour
+	unsigned int getMonth() const; // return minute
+	unsigned int getDay() const; // return second
+
+	void printUniversal() const; // BC date in universal-time format
+	void printStandard() const; // R.O.C date time in standard-time format
+private:
+	unsigned int year; // 0 - 23 (24-hour clock format)
+	unsigned int month; // 0 - 59
+	unsigned int day; // 0 - 59
+}; // end class Time
+
+#endif#pragma once
