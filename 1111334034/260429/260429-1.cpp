afb7572fde1e9b5a7af956e2769112ab0ef651ff
1111334034 <232752682+kevinlin227@users.noreply.github.com>
2026-04-29 15:50:58 +0800

    朋友類別/函數

---
+++ 1111334034/260429/260429-1.cpp
@@ -0,0 +1,50 @@
+// Fig. 9.22: fig09_22.cpp  
+// Friends can access private members of a class.
+#include <iostream>
+#include "Count.h"
+using namespace std;
+
+// printDate class definition
+class printDate
+{
+public:
+   void printX( Count &c )
+   {
+      cout << c.x << endl;
+   } // end function printX
+}; // end class printDate
+
+// function setX can modify private data of Count
+// because setX is declared as a friend of Count (line 9)
+void setX( Count &c, int val )
+{
+   c.x = val; // allowed because setX is a friend of Count
+} // end function setX
+
+int main()
+{
+   Count counter; // create Count object
+   printDate pd; // create printDate object
+
+   cout << "counter.x after instantiation: ";
+   pd.printX( counter );
+
+   setX( counter, 8 ); // set x using a friend function
+   cout << "counter.x after call to setX friend function: ";
+   pd.printX( counter );
+} // end main
+
+/**************************************************************************
+ * (C) Copyright 1992-2012 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ **************************************************************************/
