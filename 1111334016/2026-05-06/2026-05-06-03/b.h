571e4ddf7962ba847dfbededf5b7195a70b6ba77
1111334016 <liangsu168888@gmail.com>
2026-05-06 15:20:46 +0800

    Create b.h

---
+++ 1111334016/2026-05-06/2026-05-06-03/b.h
@@ -0,0 +1,51 @@
+#pragma once
+class Matrix
+{
+public:
+    int row, column;
+    int** element;
+    Matrix(int m, int n)
+    {
+        row = m;
+        column = n;
+
+        // 動態配置mxn的二維陣列element之記憶體
+        element = new int* [m];
+        int i;
+        for (i = 0; i < m; i++)
+            element[i] = new int[n];
+    }
+
+    Matrix(char name, int m, int n)
+    {
+        row = m;
+        column = n;
+
+        // 動態配置mxn的二維陣列element之記憶體
+        element = new int* [m];
+        int i, j;
+        for (i = 0; i < m; i++)
+            element[i] = new int[n];
+
+        cout << "輸入一" << m << 'x' << n << "矩陣" << name << endl;
+        for (i = 0; i < m; i++)
+            for (j = 0; j < n; j++)
+            {
+                cout << name << '[' << i << "]["
+                    << j << "]=";
+                cin >> element[i][j];
+            }
+    }
+
+    // 定義矩陣的「+」(加號)運算子的多載
+    Matrix operator-(int b)
+    {
+        int i, j;
+        Matrix temp(this->row, this->column);
+        for (i = 0; i < this->row; i++)
+            for (j = 0; j < this->column; j++)
+                temp.element[i][j] = this->element[i][j] -b;
+
+        return temp;
+    }
+};
